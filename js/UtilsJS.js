f34bb5b90a33a56e3f3d1394aa8b7c07630de81f
Pablo Martínez <pabloextreme27@gmail.com>
2024-06-09 13:13:07 +0000

    onChangeEnd

--- js/UtilsJS.js
+++ js/UtilsJS.js
@@ -1,13 +1,21 @@
 export function animateNumber(currentValue, newValue, callback, duration = 1000) {
-    const frameDuration = 1000 / 60;
-    const totalFrames = Math.round(duration / frameDuration);
-    const increment = (newValue - currentValue) / totalFrames;
-    let currentFrame = 0;
+  const frameDuration = 1000 / 60;
+  const totalFrames = Math.round(duration / frameDuration);
+  const increment = (newValue - currentValue) / totalFrames;
+  let currentFrame = 0;
 
-    const interval = setInterval(() => {
-        currentFrame++;
-        const currentValueToDisplay = Math.round(currentValue + increment * currentFrame);
-        callback(currentFrame === totalFrames ? newValue : currentValueToDisplay);
-        if (currentFrame === totalFrames) clearInterval(interval);
-    }, frameDuration);
+  const interval = setInterval(() => {
+    currentFrame++;
+    const currentValueToDisplay = Math.round(currentValue + increment * currentFrame);
+    callback(currentFrame === totalFrames ? newValue : currentValueToDisplay);
+    if (currentFrame === totalFrames) clearInterval(interval);
+  }, frameDuration);
+}
+
+export function onChangeEnd(callback, delay) {
+  let timeoutId;
+  return function () {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(callback, delay);
+  };
 }
